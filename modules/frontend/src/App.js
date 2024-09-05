e941308c004052edd389ca7a603e9dfaf06cda8b
HumaMilisic <HumaMilisic@users.noreply.github.com>
2024-09-05 10:08:04 +0200

    feat: env variables

--- modules/frontend/src/App.js
+++ modules/frontend/src/App.js
@@ -4,14 +4,15 @@ import logo from "./images/UdaConnectLogo.svg";
 import Persons from "./components/Persons";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="header">
-        <img src={logo} className="App-logo" alt="UdaConnect" />
-      </div>
-      <Persons />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="header">
+                <img src={logo} className="App-logo" alt="UdaConnect"/>
+            </div>
+            <pre>{process.env.REACT_APP_API_KEY}</pre>
+            <Persons/>
+        </div>
+    );
 }
 
 export default App;
